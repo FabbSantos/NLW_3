1b4b64408c2abe7e81ee9e67658c29aaba1cd704
Fabricio <fabriciobs2000@gmail.com>
2020-10-13 20:47:19 -0300

    i'm stupid. It's working now.

--- aulas/web/src/pages/OrphanagesMap.tsx
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -34,7 +34,7 @@ function OrphanagesMap () {
             >
                 
                 <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MABPOX_TOKEN}`} 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
                 />
             </Map>
 
