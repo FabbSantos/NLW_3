e2f754d512169fb1b39c51e57c9cea204997a02a
Fabricio <fabriciobs2000@gmail.com>
2020-10-13 02:38:09 -0300

    map page added and concluded. map for some reason isn't working.

--- aulas/web/src/pages/OrphanagesMap.tsx
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import MapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+// import { tileLayer } from 'leaflet';
+
 
 function OrphanagesMap () {
     return(
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={ MapMarkerImg } alt="Happy" />
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita =)</p>
+                </header>
+                
+                <footer>
+                    <strong>Rio de Janeiro</strong>
+                    <span>Rio de Janeiro</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-22.9311479,-43.3584274]}
+                zoom={15}
+                style={{ width:'100%', height: '100%' }}
+            >
+                
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MABPOX_TOKEN}`} 
+                />
+            </Map>
+
+            <Link to ="" className="create-orphanage"> 
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     );
 };
 
